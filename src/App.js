6be4edf24ada0475d763b83e36b449e5ba8cfe69
carlosgomez <carlos.gomez@electric.ai>
2022-06-20 19:49:28 -0400

    hey

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the new App.js!
+          This is a a test...
         </p>
         <a
           className="App-link"
